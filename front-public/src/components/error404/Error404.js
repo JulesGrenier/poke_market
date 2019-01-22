ba8caca0a8b5f7fbedf77e68dcd773168ba4d5af
Jules Grenier <grenier.jules@outlook.com>
2019-01-22 16:25:37 +0100

    error 404 page update

--- front-public/src/components/error404/Error404.js
+++ front-public/src/components/error404/Error404.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
+import '../../styles/error404/error404.scss';
 
 const Error404 = () => {
   return (
     <div id="error-404">
-      <h2>404</h2>
-      <p>The page you are looking for does not exist</p>
+      <Container text textAlign="center">
+        <img src={require('../../images/sad-pikachu.png')} alt="sad pikachy"/>
+        <h2>404</h2>
+        <p>The page you are looking for does not exist</p>
+      </Container>
     </div>
   );
 };
