3de0bc8129cc40a450dfd4f5594238c7a0e82929
Jules Grenier <grenier.jules@outlook.com>
2019-01-26 23:24:18 +0100

    PokéAPI credits added + parsing erros fixed

--- front-public/src/components/home/Home.js
+++ front-public/src/components/home/Home.js
@@ -2,25 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Button } from 'semantic-ui-react';
 import '../../styles/home/home.scss';
+import pokecoin from '../../images/shop-bag.svg';
 
-const Home = () => {
-  return (
-    <div id="home">
-      <div className="background">
-        <div className="image"></div>
-      </div>
-      <Container text textAlign="center">
-        <img src={require("../../images/shop-bag.svg")} alt="shop-bag"/>
-        <h1>Poke Market</h1>
-        <p>Browse our large list of pokemon items.
-          <br />There must be something you want, or will want !
-        </p>
-        <Link to="/browse">
-          <Button>Browse</Button>
-        </Link>
-      </Container>
+const Home = () => (
+  <div id="home">
+    <div className="background">
+      <div className="image" />
     </div>
-  );
-};
+    <Container text textAlign="center">
+      <img src={pokecoin} alt="shop-bag" />
+      <h1>Poke Market</h1>
+      <p>Browse our large list of pokemon items from the official PokéAPI.
+        <br />There must be something you want, or will want !
+      </p>
+      <Link to="/browse">
+        <Button>Browse</Button>
+      </Link>
+    </Container>
+  </div>
+);
 
 export default Home;
--- front-public/src/containers/home/HomeContainer.js
+++ front-public/src/containers/home/HomeContainer.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import Home from '../../components/home/Home';
 
 class HomeContainer extends Component {
-
-  componentWillMount(){
-    document.title = "Poké Market";
+  componentWillMount() {
+    document.title = 'Poké Market';
   }
 
   render() {
