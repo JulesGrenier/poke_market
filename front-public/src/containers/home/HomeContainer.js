65b824309cb728792540e4c0584fc7f7ed62c698
Jules Grenier <grenier.jules@outlook.com>
2019-01-26 03:41:40 +0100

    document title changed on Home loading

--- front-public/src/containers/home/HomeContainer.js
+++ front-public/src/containers/home/HomeContainer.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Home from '../../components/home/Home';
 
 class HomeContainer extends Component {
+
+  componentWillMount(){
+    document.title = "Poké Market";
+  }
+
   render() {
     return (
       <Home />
