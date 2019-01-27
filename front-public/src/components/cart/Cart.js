41678206c34beaba857ad1652eaedc7ef25d839b
Jules Grenier <grenier.jules@outlook.com>
2019-01-27 03:31:08 +0100

    buttons validate cart & empty cart added

--- front-public/src/components/cart/Cart.js
+++ front-public/src/components/cart/Cart.js
@@ -1,29 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Icon, Grid } from 'semantic-ui-react';
 import CartItem from './CartItem';
 import '../../styles/cart/cart.scss';
 
-const Cart = ({ items }) => {
-  return (
-    <div id="cart">
-      <Container text>
-        <h2><Icon name="cart" /> My Cart</h2>
+const Cart = ({ items }) => (
+  <div id="cart">
+    <Container text>
+      <h2><Icon name="cart" /> My Cart</h2>
 
-        <div className="cart-items">
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={16}>
-                <CartItem />
-              </Grid.Column>
-              <Grid.Column width={16}>
-                <CartItem />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-      </Container>
-    </div>
-  );
-};
+      <div className="cart-items">
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={16}>
+              <CartItem />
+            </Grid.Column>
+            <Grid.Column width={16}>
+              <CartItem />
+            </Grid.Column>
+            <Grid.Column width={16}>
+              <Link to="/home" className="validate-order-btn">Validate My Order</Link>
+              <Link to="/home" className="empty-cart-btn">Empty My Cart</Link>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    </Container>
+  </div>
+);
 
 export default Cart;
